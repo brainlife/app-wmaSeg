e785e13445f0c26626843f133092fdbf4abed9ca
kitchell <lindseykitchell21@gmail.com>
2017-09-18 12:15:24 -0400

    initial files

---
+++ main.m
@@ -0,0 +1,70 @@
+function [] = main()
+
+switch getenv('ENV')
+    case 'IUHPC'
+        disp('loading paths for IUHPC')
+        addpath(genpath('/N/u/hayashis/BigRed2/git/encode'))
+        addpath(genpath('/N/u/hayashis/BigRed2/git/vistasoft'))
+        addpath(genpath('/N/u/hayashis/BigRed2/git/jsonlab'))
+        addpath(genpath('/N/u/hayashis/BigRed2/git/wma'))
+    case 'VM'
+        disp('loading paths for Jetstream VM')
+        addpath(genpath('/usr/local/encode'))
+        addpath(genpath('/usr/local/vistasoft'))
+        addpath(genpath('/usr/local/jsonlab'))
+        addpath(genpath('/usr/local/wma'))
+end
+
+
+% load my own config.json
+config = loadjson('config.json');
+
+% Load an FE strcuture created by the sca-service-life
+load(config.fe);
+
+% point to dt6 file
+dt6 = fullfile(config.dt6,'/dt6.mat')
+
+% run wma
+classification = wma_wrapper(fe, dt6, config.freesurfer);
+
+%remove classified zero-weighted fibers
+classification = wma_clearNonvalidClassifications(classification,fe);
+
+% make fg_classified
+
+fg_classified = bsc_makeFGsFromClassification(classification, fe);
+
+save('output.mat','fg_classified', 'classification', '-v7.3');
+
+tracts = fg2Array(fg_classified);
+
+mkdir('tracts');
+
+% Make colors for the tracts
+cm = parula(length(tracts));
+for it = 1:length(tracts)
+   tract.name   = tracts(it).name;
+   tract.color  = cm(it,:);
+   tract.coords = tracts(it).fibers;
+   savejson('', tract, fullfile('tracts',sprintf('%i.json',it)));
+   clear tract
+end
+
+% saving text file with number of fibers per tracts
+tract_info = cell(length(fg_classified), 2);
+
+for i = 1:length(fg_classified)
+    tract_info{i,1} = fg_classified(i).name;
+    tract_info{i,2} = length(fg_classified(i).fibers);
+end
+
+T = cell2table(tract_info);
+T.Properties.VariableNames = {'Tracts', 'FiberCount'};
+
+writetable(T,'output_fibercounts.txt')
+
+end
+
+
+
