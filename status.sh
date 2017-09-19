ddb14132660334e8b959a15082a8bf0f9606f3da
Soichi Hayashi <hayashis@iu.edu>
2017-09-18 20:22:07 -0400

    added .log to the stdout/error so that they can be opened with a single click on brain-life

--- status.sh
+++ status.sh
@@ -33,7 +33,7 @@ if [ -f jobid ]; then
     fi
     if [ $jobstate == "R" ]; then
         subid=$(cat jobid | cut -d '.' -f 1)
-        logname="wmaSeg.o$subid"
+        logname="wmaSeg.o$subid.log"
         tail -1 $logname
         exit 0
     fi
