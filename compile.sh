b645b8ae8fa19f5fd9202b00037db4f7550a8442
Soichi Hayashi <hayashis@iu.edu>
2018-06-26 16:37:38 -0400

    added commit_ids

--- compile.sh
+++ compile.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 module load matlab/2017a
 
+log=compiled/commit_ids.txt
+true > $log
+echo "/N/u/brlife/git/encode" >> $log
+(cd /N/u/brlife/git/encode && git log -1) >> $log
+echo "/N/u/brlife/git/vistasoft" >> $log
+(cd /N/u/brlife/git/vistasoft && git log -1) >> $log
+echo "/N/u/brlife/git/jsonlab" >> $log
+(cd /N/u/brlife/git/jsonlab && git log -1) >> $log
+echo "/N/u/kitchell/Karst/Applications/mba" >> $log
+(cd /N/u/kitchell/Karst/Applications/mba && git log -1) >> $log
+echo "/N/u/brlife/git/wma" >> $log
+(cd /N/u/brlife/git/wma && git log -1) >> $log
+
 cat > build.m <<END
 addpath(genpath('/N/u/brlife/git/encode'))
 addpath(genpath('/N/u/brlife/git/vistasoft'))
