b678cf9b0dc236fd0aad67c87e2f57d7c818be99
Daniel Bullock <dnbulloc@i16.red.uits.iu.edu>
2019-01-31 19:37:42 -0500

    version 1.5 upload

--- compile.sh
+++ compile.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
+module unload matlab
 module load matlab/2017a
 
 log=compiled/commit_ids.txt
 true > $log
-echo "/N/u/brlife/git/encode" >> $log
-(cd /N/u/brlife/git/encode && git log -1) >> $log
 echo "/N/u/brlife/git/vistasoft" >> $log
 (cd /N/u/brlife/git/vistasoft && git log -1) >> $log
 echo "/N/u/brlife/git/jsonlab" >> $log
 (cd /N/u/brlife/git/jsonlab && git log -1) >> $log
-echo "/N/u/kitchell/Karst/Applications/mba" >> $log
-(cd /N/u/kitchell/Karst/Applications/mba && git log -1) >> $log
-echo "/N/u/brlife/git/wma" >> $log
-(cd /N/u/brlife/git/wma && git log -1) >> $log
+echo "/N/u/brlife/git/wma_tools" >> $log
+(cd /N/u/brlife/git/wma_tools && git log -1) >> $log
+echo "/N/u/brlife/git/encode" >> $log
+(cd /N/u/brlife/git/encode && git log -1) >> $log
+#echo "/N/u/brlife/git/encode" >> $log
+#(cd /N/u/brlife/git/encode && git log -1) >> $log
+#echo "/N/u/brlife/git/spm12" >> $log
+#(cd /N/u/brlife/git/spm12 && git log -1) >> $log
 
 cat > build.m <<END
-addpath(genpath('/N/u/brlife/git/encode'))
 addpath(genpath('/N/u/brlife/git/vistasoft'))
 addpath(genpath('/N/u/brlife/git/jsonlab'))
-addpath(genpath('/N/soft/mason/SPM/spm8'))
+addpath(genpath('/N/u/brlife/git/wma_tools'))
+addpath(genpath('/N/u/brlife/git/encode'))
+mcc -m -R -nodisplay -d compiled wma_segMajTracks_BL
 
-addpath(genpath('/N/u/kitchell/Karst/Applications/mba'))
-addpath(genpath('/N/u/brlife/git/wma'))
-
-mcc -m -R -nodisplay -d compiled main
 exit
 END
+
 matlab -nodisplay -nosplash -r build && rm build.m
+
